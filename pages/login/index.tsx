74cc9caa9cd511bd06fbdcd90fe3309879b2fe3d
Wibus <1596355173@qq.com>
2022-01-19 21:06:11 +0800

    fix(login): push LoginView into default export

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,13 +3,15 @@
  * @author: Wibus
  * @Date: 2022-01-19 20:37:52
  * @LastEditors: Wibus
- * @LastEditTime: 2022-01-19 21:02:51
+ * @LastEditTime: 2022-01-19 21:05:31
  * Coding With IU
  */
 
 import { NextPage } from "next"
-export const LoginView: NextPage = () => {
+const LoginView: NextPage = () => {
   return (
     <></>
   )
 }
+
+export default LoginView
