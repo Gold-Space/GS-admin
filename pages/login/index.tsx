4a2d93b16a2e54c3afabf28be8d9c8576d52b8c0
Wibus <1596355173@qq.com>
2022-01-19 21:03:15 +0800

    fix(login): change the type of LoginView

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,12 +1,14 @@
 /*
- * @FilePath: /GS-admin/pages/login.tsx
+ * @FilePath: /GS-admin/pages/login/index.tsx
  * @author: Wibus
  * @Date: 2022-01-19 20:37:52
  * @LastEditors: Wibus
- * @LastEditTime: 2022-01-19 20:37:52
+ * @LastEditTime: 2022-01-19 21:02:51
  * Coding With IU
  */
-export const Login = () => {
+
+import { NextPage } from "next"
+export const LoginView: NextPage = () => {
   return (
     <></>
   )
